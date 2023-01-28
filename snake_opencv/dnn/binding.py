621ec4e80de8235d5393dc7d22d87e00c67f1235
cospectrum <severinalexeyv@gmail.com>
2023-01-28 23:58:18 +0300

    Add blob_from_images

--- snake_opencv/dnn/binding.py
+++ snake_opencv/dnn/binding.py
@@ -2,13 +2,14 @@ import cv2
 import numpy as np
 
 
-from typing import Optional, Tuple
+from typing import List, Optional, Tuple
 
 from ..core import CV_32F
 
 
 __all__ = [
     'blob_from_image',
+    'blob_from_images',
 ]
 
 
@@ -23,6 +24,26 @@ def blob_from_image(
 ) -> np.ndarray:
     return cv2.dnn.blobFromImage(
         image,
+        scalefactor,
+        size,
+        mean=mean,
+        swapRB=swap_rb,
+        crop=crop,
+        ddepth=ddepth,
+    )
+
+
+def blob_from_images(
+    images: List[np.ndarray],
+    scalefactor: float = 1.0,
+    size: Optional[Tuple[int, int]] = None,
+    mean: Optional[Tuple[float, ...]] = None,
+    swap_rb: bool = False,
+    crop: bool = False,
+    ddepth: int = CV_32F,
+) -> np.ndarray:
+    return cv2.dnn.blobFromImages(
+        images,
         scalefactor,
         size,
         mean=mean,
--- tests/test_dnn.py
+++ tests/test_dnn.py
@@ -11,3 +11,13 @@ def test_blob_from_image() -> None:
     left = cv.dnn.blob_from_image(image)
     right = cv2.dnn.blobFromImage(image)
     assert eq(left, right)
+
+
+def test_blob_from_images() -> None:
+    images = [
+        random_image(height=45, width=45)
+        for _ in range(3)
+    ]
+    left = cv.dnn.blob_from_images(images)
+    right = cv2.dnn.blobFromImages(images)
+    assert eq(left, right)
