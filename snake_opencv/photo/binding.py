fed916130060d339e8e3ad1ea09aeff26bd8ccef
cospectrum <severinalexeyv@gmail.com>
2023-01-30 03:14:26 +0300

    Add fast_nl_means_denoising (photo)

--- snake_opencv/photo/binding.py
+++ snake_opencv/photo/binding.py
@@ -5,8 +5,52 @@ from typing import Optional
 
 
 __all__ = [
+    'fast_nl_means_denoising',
     'fast_nl_means_denoising_colored',
 ]
+
+
+def fast_nl_means_denoising(
+    src: np.ndarray,
+    dst: Optional[np.ndarray] = None,
+    h: float = 3.0,
+    template_window_size: int = 7,
+    search_window_size: int = 21,
+) -> np.ndarray:
+    """Perform image denoising using Non-local Means Denoising algorithm
+    http://www.ipol.im/pub/algo/bcm_non_local_means_denoising/ with several
+    computational optimizations. Noise expected to be a gaussian white noise.
+
+    This function expected to be applied to grayscale images. For colored
+    images look at fast_nl_means_denoising_colored. Advanced usage of this
+    functions can be manual denoising of colored image in different
+    colorspaces. Such approach is used in fast_nl_means_denoising_colored by
+    converting image to CIELAB colorspace and then separately denoise L and AB
+    components with different h parameter.
+
+    Args:
+        src: Input 8-bit 1-channel, 2-channel, 3-channel or 4-channel image.
+        dst: Output image with the same size and type as src.
+        h:
+            Parameter regulating filter strength. Big h value perfectly
+            removes noise but also removes image details, smaller h value
+            preserves details but also preserves some noise
+        template_window_size:
+            Size in pixels of the template patch that is used to compute
+            weights. Should be odd. Recommended value 7 pixels
+        search_window_size:
+            Size in pixels of the window that is used to compute weighted
+            average for given pixel. Should be odd. Affect performance
+            linearly: greater search_windows_size - greater denoising time.
+            Recommended value 21 pixels
+    """
+    return cv2.fastNlMeansDenoising(
+        src,
+        dst,
+        h,
+        template_window_size,
+        search_window_size,
+    )
 
 
 def fast_nl_means_denoising_colored(
@@ -31,7 +75,7 @@ def fast_nl_means_denoising_colored(
             Bigger h value perfectly removes noise but also removes image
             details, smaller h value preserves details but also preserves some
             noise
-        hColor:
+        h_color:
             The same as h but for color components. For most images value
             equals 10 will be enough to remove colored noise and do not
             distort colors
@@ -41,7 +85,7 @@ def fast_nl_means_denoising_colored(
         search_window_size:
             Size in pixels of the window that is used to compute weighted
             average for given pixel. Should be odd. Affect performance
-            linearly: greater searchWindowsSize - greater denoising time.
+            linearly: greater search_windows_size - greater denoising time.
             Recommended value 21 pixels
     """
     return cv2.fastNlMeansDenoisingColored(
--- tests/test_photo.py
+++ tests/test_photo.py
@@ -11,3 +11,11 @@ def test_fast_nl_means_denoising_colored() -> None:
     left = cv.fast_nl_means_denoising_colored(image)
     right = cv2.fastNlMeansDenoisingColored(image)
     assert eq(left, right)
+
+
+def test_fast_nl_means_denoising() -> None:
+    image = random_image(height=80, width=40)
+
+    left = cv.fast_nl_means_denoising(image)
+    right = cv2.fastNlMeansDenoising(image)
+    assert eq(left, right)
