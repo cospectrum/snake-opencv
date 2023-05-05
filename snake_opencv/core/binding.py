6b66f4a406ceba84e8c57388ada5b9fc4a780e04
cospectrum <severinalexeyv@gmail.com>
2023-05-06 02:27:24 +0300

    add xor, or, not, absdiff, add, add_weighted

--- snake_opencv/core/binding.py
+++ snake_opencv/core/binding.py
@@ -5,8 +5,69 @@ from typing import Optional
 
 
 __all__ = [
+    'absdiff',
+    'add',
+    'add_weighted',
+    'bitwise_and',
+    'bitwise_not',
     'bitwise_or',
+    'bitwise_xor',
 ]
+
+
+def absdiff(
+    src1: np.ndarray,
+    src2: np.ndarray,
+    dst: Optional[np.ndarray] = None,
+) -> np.ndarray:
+    return cv2.absdiff(src1, src2, dst=dst)
+
+
+def add(
+    src1: np.ndarray,
+    src2: np.ndarray,
+    dst: Optional[np.ndarray] = None,
+    mask: Optional[np.ndarray] = None,
+    dtype: int = -1,
+) -> np.ndarray:
+    return cv2.add(src1, src2, dst=dst, mask=mask, dtype=dtype)
+
+
+def add_weighted(
+    src1: np.ndarray,
+    alpha: float,
+    src2: np.ndarray,
+    beta: float,
+    gamma: float,
+    dst: Optional[np.ndarray] = None,
+    dtype: int = -1,
+) -> np.ndarray:
+    return cv2.addWeighted(
+        src1,
+        alpha=alpha,
+        src2=src2,
+        beta=beta,
+        gamma=gamma,
+        dst=dst,
+        dtype=dtype,
+    )
+
+
+def bitwise_and(
+    src1: np.ndarray,
+    src2: np.ndarray,
+    dst: Optional[np.ndarray] = None,
+    mask: Optional[np.ndarray] = None,
+) -> np.ndarray:
+    return cv2.bitwise_and(src1, src2, dst=dst, mask=mask)
+
+
+def bitwise_not(
+    src: np.ndarray,
+    dst: Optional[np.ndarray] = None,
+    mask: Optional[np.ndarray] = None,
+) -> np.ndarray:
+    return cv2.bitwise_not(src, dst=dst, mask=mask)
 
 
 def bitwise_or(
@@ -21,17 +82,17 @@ def bitwise_or(
     The function cv::bitwise_or calculates the per-element bit-wise logical
     disjunction for: Two arrays when src1 and src2 have the same size:
 
-        dst(I)=src1(I)∨src2(I)if mask(I)≠0
+        dst(I) = src1(I) ∨ src2(I)  if mask(I)≠0
 
     An array and a scalar when src2 is constructed from Scalar or has the same
     number of elements as src1.channels():
 
-        dst(I)=src1(I)∨src2if mask(I)≠0
+        dst(I) = src1(I) ∨ src2  if mask(I)≠0
 
     A scalar and an array when src1 is constructed from Scalar or has the same
     number of elements as src2.channels():
 
-        dst(I)=src1∨src2(I)if mask(I)≠0
+        dst(I) = src1 ∨ src2(I)  if mask(I)≠0
 
     In case of floating-point arrays, their machine-specific bit
     representations (usually IEEE754-compliant) are used for the operation.
@@ -48,3 +109,12 @@ def bitwise_or(
             elements of the output array to be changed.
     """
     return cv2.bitwise_or(src1, src2, dst=dst, mask=mask)
+
+
+def bitwise_xor(
+    src1: np.ndarray,
+    src2: np.ndarray,
+    dst: Optional[np.ndarray] = None,
+    mask: Optional[np.ndarray] = None,
+) -> np.ndarray:
+    return cv2.bitwise_xor(src1, src2, dst=dst, mask=mask)
--- tests/test_core.py
+++ tests/test_core.py
@@ -8,14 +8,53 @@ import numpy as np
 from .utils import eq, random_image
 
 
+w = random.randint(1, 1000)
+h = random.randint(1, 1000)
+
+img1 = random_image(h, w)
+img2 = random_image(h, w)
+
+
+def test_absdiff() -> None:
+    left = cv.absdiff(img1, img2)
+    right = cv2.absdiff(img1, img2)
+    assert eq(left, right)
+
+
+def test_add() -> None:
+    left = cv.add(img1, img2)
+    right = cv2.add(img1, img2)
+    assert eq(left, right)
+
+
+def test_add_weighted() -> None:
+    alpha = random.random()
+    beta = random.random()
+    gamma = random.random()
+    left = cv.add_weighted(img1, alpha, img2, beta, gamma)
+    right = cv2.addWeighted(img1, alpha, img2, beta, gamma)
+    assert eq(left, right)
+
+
+def test_bitwise_and() -> None:
+    left = cv.bitwise_and(img1, img2)
+    right = cv2.bitwise_and(img1, img2)
+    assert eq(left, right)
+
+
+def test_bitwise_not() -> None:
+    left = cv.bitwise_not(img1)
+    right = cv2.bitwise_not(img1)
+    assert eq(left, right)
+
+
 def test_bitwise_or() -> None:
-    w = random.randint(1, 1000)
-    h = random.randint(1, 1000)
-
-    img1 = random_image(h, w)
-    img2 = random_image(h, w)
-
     left = cv.bitwise_or(img1, img2)
     right = cv2.bitwise_or(img1, img2)
+    assert eq(left, right)
 
+
+def test_bitwise_xor() -> None:
+    left = cv.bitwise_xor(img1, img2)
+    right = cv2.bitwise_xor(img1, img2)
     assert eq(left, right)
